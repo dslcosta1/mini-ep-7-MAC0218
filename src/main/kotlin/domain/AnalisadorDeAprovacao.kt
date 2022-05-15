a6d9a08b27aab78aa8df78de4b2e2d9e999343bb
Daniel Silva Lopes da Costa <dslcosta2016@usp.br>
2022-05-15 12:25:38 -0300

    Add methods to AnalisadorDeAprovacao Class

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,4 +1,7 @@
 package domain
+import domain.criterios.CriterioDeAprovacao
+import domain.criterios.Basico
+
 
 class AnalisadorDeAprovacao {
 
@@ -11,5 +14,18 @@ class AnalisadorDeAprovacao {
     //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
     //
     // ---------------------------------
+    
+    var criterioAnalisador : CriterioDeAprovacao = Basico()
 
+    fun defineCriterio(criterio : CriterioDeAprovacao) {
+        criterioAnalisador = criterio
+    }
+
+    fun fechaBoletim(boletim : Boletim) : BoletimFechado{
+        val mediaFinal : Double = criterioAnalisador.mediaFinal(boletim);
+        val estaAprovado : Boolean = criterioAnalisador.estaAprovado(boletim);
+
+        return BoletimFechado(mediaEPs = boletim.mediaEPs, mediaMiniEPs = boletim.mediaMiniEPs, mediaFinal = mediaFinal, foiAprovado = estaAprovado)
+
+    }
 }
